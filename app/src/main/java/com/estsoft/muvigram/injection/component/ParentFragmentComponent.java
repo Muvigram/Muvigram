5adff109660602b61b5ccdecb6ae35ec0720f87d
Gong Ui Jang <dalkomsft02@gmail.com>
2016-11-08 16:57:40 +0900

    bf

--- app/src/main/java/com/estsoft/muvigram/injection/component/ParentFragmentComponent.java
+++ app/src/main/java/com/estsoft/muvigram/injection/component/ParentFragmentComponent.java
@@ -5,6 +5,7 @@ import com.estsoft.muvigram.injection.module.NestedFragmentModule;
 import com.estsoft.muvigram.injection.module.ParentFragmentModule;
 import com.estsoft.muvigram.ui.feed.FeedFragment;
 import com.estsoft.muvigram.ui.musicselect.pager.MusicSelectFragment;
+import com.estsoft.muvigram.ui.search.SearchFragment;
 
 import dagger.Subcomponent;
 
